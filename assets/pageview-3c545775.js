111c605097b182dd80e4c7df9610885798745674
brinishness <596231290@qq.com>
2023-04-21 20:58:22 +0000

    Deploying to gh-pages from @ brinishness/brinishness.github.io@9043030e85b5da4a7fe1e0c755a6f8f68af51d42 🚀

--- assets/pageview-3c545775.js
+++
@@ -1,1 +0,0 @@
-import{i as e}from"./app-5bf4d2fa.js";import"./framework-3e01730a.js";const i=()=>e({serverURL:"https://waline.brinishness.eu.org"});export{i as updatePageview};
---
+++ assets/pageview-dae4a16d.js
@@ -0,0 +1,1 @@
+import{i as e}from"./app-dc0c5fce.js";import"./framework-3e01730a.js";const a=()=>e({serverURL:"http://waline.brinishness.eu.org:8980"});export{a as updatePageview};
